782be5faa74fcfd8b3e52b0618d55728ff54bbc3
likhit <likhitb2003@gmail.com>
2023-10-01 22:34:04 +0530

    second commit

---
+++ Optional_operator.js
@@ -0,0 +1,6 @@
+a={
+    my:'123',
+    news:{hello:'1233',pics:'445'}
+}
+console.log(a?.news?.hello)
+// the main differnece b/w optional operator and ${} is optional gives undefined in case of null values 
